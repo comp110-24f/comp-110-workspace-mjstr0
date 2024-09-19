8d44ad36f9d340532caa5999d5c2c6d5fdd5302e
mjstr0 <mjstr@unc.edu>
2024-09-19 12:22:22 -0400

    conditionals practice

--- lessons/conditionals_2.py
+++ lessons/conditionals_2.py
@@ -1,4 +1,13 @@
-def check_first_letter(word: str, letter: str) -> str:
-    """check if letter is first character of word"""
-    word = letter 
-    return: match
+def number_info(num: int) -> int:
+    if num < 10:
+        print("Small number.")
+    else:
+        if num % 2 == 0:
+            print("Even number.")
+        else:
+            print("Odd number.")
+    return num
+
+
+number_info(num=11)
+print(number_info(num=4))
